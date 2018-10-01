ff8911a8e915b1f6751952899a8a1e5c38836d0d
ludianq <43664222+ludianq@users.noreply.github.com>
2018-10-01 01:29:01 -0500

    project creation

---
+++ Oprava.Common/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oprava.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public  class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Names { get; set; }
+        [Required]
+        public string Contact { get; set; }
+        [Required]
+        public string Profesion { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishOn { get; set; }
+
+    }
+}
---
+++ Oprava.Domain/Models/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oprava.Domain.Models
+{
+    using System.Data.Entity;
+    public  class DataContext : DbContext
+    {
+
+        //Constructor de la base de datos
+        public DataContext() : base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<Oprava.Common.Models.Product> Products { get; set; }
+    }
+}
