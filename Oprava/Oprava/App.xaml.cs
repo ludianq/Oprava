6c55174618d6bd07805b33ca3b15ac0854785f19
ludianq <43664222+ludianq@users.noreply.github.com>
2018-10-03 23:42:12 -0500

    project creation

--- Oprava/Oprava/App.xaml.cs
+++ Oprava/Oprava/App.xaml.cs
@@ -5,13 +5,15 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Oprava
 {
+
+    using Views;
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ProductsPages();
         }
 
         protected override void OnStart()
---
+++ Oprava/Oprava/Infractructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oprava.Infractructure
+{
+    using ViewModels;
+
+  public  class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+
+   
+
+}
